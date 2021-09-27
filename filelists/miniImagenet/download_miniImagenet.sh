5e769a16c45f12bbf837a94490399aa5a380468e
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-09-27 15:01:09 +0330

    fix imagenet path

--- filelists/miniImagenet/download_miniImagenet.sh
+++ filelists/miniImagenet/download_miniImagenet.sh
@@ -3,6 +3,6 @@
 # wget https://raw.githubusercontent.com/twitter/meta-learning-lstm/master/data/miniImagenet/val.csv 
 # wget https://raw.githubusercontent.com/twitter/meta-learning-lstm/master/data/miniImagenet/test.csv
 # wget http://image-net.org/image/ILSVRC2015/ILSVRC2015_CLS-LOC.tar.gz 
-tar -zxvf ../../../imagenet_object_localization_patched2019.tar.gz
+tar -zxvf imagenet_object_localization_patched2019.tar.gz
 python write_miniImagenet_filelist.py
 python write_cross_filelist.py
